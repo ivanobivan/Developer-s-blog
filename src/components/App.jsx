5743e1dd04cc0735c7248b03dfacaddd6861ad0d
ivanobivan <ikolesov20091@bk.ru>
2018-01-31 00:59:56 +0300

    create simple components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Toolbar from './Toolbar'
 import '../less/index.less'
 
 export default class App extends React.Component {
@@ -6,6 +7,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
+                <Toolbar/>
                 <p>My first developer's blog</p>
             </div>
         )
---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Toolbar extends React.Component {
+
+    render() {
+        return(
+            <nav>
+                <a href="#">Development process</a>
+                <a href="#">Feedback</a>
+            </nav>
+        )
+    }
+}
