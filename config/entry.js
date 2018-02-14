cde27774d49fc21bd21f0900861e560afed44cb8
ivanobivan <ikolesov20091@bk.ru>
2018-02-14 19:59:08 +0300

    refactor redux part

--- config/entry.js
+++
@@ -1,9 +0,0 @@
-const env = require("./environment");
-const path = require("path");
-
-module.exports = [
-    "react-hot-loader/patch",
-    "webpack-hot-middleware/client",
-    path.join(__dirname, "../src/less/index.less"),
-    path.join(__dirname, "../src/app.jsx")
-];
--- config/environment.js
+++
@@ -1,7 +0,0 @@
-const ENVIRONMENT = process.env.NODE_ENV || 'development';
-const IS_PRODUCTION = ENVIRONMENT === 'production';
-
-module.exports = {
-    ENVIRONMENT,
-    IS_PRODUCTION
-};
