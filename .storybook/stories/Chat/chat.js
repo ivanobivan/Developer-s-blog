4c6a1ce5b56c480f0f4254fbf879413356c1863b
ivan <ikolesov20091@bk.ru>
2018-03-27 00:08:47 +0300

    simple grid on chat page

--- .storybook/stories/Chat/chat.js
+++ .storybook/stories/Chat/chat.js
@@ -3,7 +3,6 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withNotes } from '@storybook/addons';
 import Chat from '../../../src/client/components/chat/Chat';
-//import '../../../src/client/less/app/chat/chat.less'
 
 storiesOf('Chat Page', module)
     .add('test', () => (
--- src/client/components/chat/Chat.jsx
+++ src/client/components/chat/Chat.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
 
 export default class Chat extends React.Component {
 
     render() {
-        return(
+        return (
             <div id="chat__root">
-                <h1>{this.props.text}</h1>
+                <div id="test">
+                    <aside className="usersPanel__chat">
+                        users
+                    </aside>
+                    <section className="chatSide__chat">
+                        <section className="messagePanel__chatSide">
+                            message
+                        </section>
+                        <section className="inputPanel__chatSide">
+                            input
+                        </section>
+                    </section>
+
+                </div>
             </div>
         )
     }
 }
+
+const mapStateToProps = state => {
+    return {};
+};
+
+const mapDispatchToProps = dispatch => {
+    return {};
+};
+
+//export default connect(mapStateToProps, mapDispatchToProps)(Chat);
