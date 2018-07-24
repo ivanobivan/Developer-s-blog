c5e617b45938e1a5ca89d9eda26fa373524bb7b6
ivan <ikolesov20091@bk.ru>
2018-07-24 10:26:49 +0300

    add react-context example

--- src/client/components/AboutMe/AboutMe.jsx
+++ src/client/components/AboutMe/AboutMe.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
-import {connect} from 'react-redux'
-import {push} from 'react-router-redux'
-import {Stack} from './Stack'
-import {Announcements} from './Announcements'
+import {ThemeContext, themes} from '../../Theme/context';
+import Button from './Button';
+import React from "react";
 
-export class AboutMe extends React.Component {
+function Toolbar(props) {
+    return (
+        <Button onClick={props.changeTheme} value='TEST'/>
+    );
+}
+
+class AboutMe extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            theme: themes.light,
+        };
+
+        this.toggleTheme = () => {
+            this.setState(state => ({
+                theme:
+                    state.theme === themes.dark
+                        ? themes.light
+                        : themes.dark,
+            }));
+        };
     }
 
     render() {
         return (
             <div id="aboutMe__root">
-                <h1>Welcome to the page about me</h1>
-                <h3>This page is under development</h3>
-                <p>I am glad to see you on this page
-                    Please, add this page in favorites and follow the development</p>
+                <ThemeContext.Provider value={this.state.theme}>
+                    <Toolbar changeTheme={this.toggleTheme}/>
+                </ThemeContext.Provider>
+                <div>
+                    <Button
+                        value='TEST'
+                    />
+                </div>
             </div>
-        )
+        );
     }
 }
 
-const mapStateToProps = state => {
-    return {};
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        push: location => dispatch(push(location))
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(AboutMe);
+export default AboutMe;
